4bba8a2401cde7b1a3340592fab6cdd488c04298
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 17:12:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(array)
+  i = 0
+  capitalized_names = []
 
-
+  while i < array.length do
+    capitalized_names << yield(array[i].capitalize)
+    i += 1
+  end
+  capitalized_names
+end
